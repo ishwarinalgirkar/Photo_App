6b31d877e644e6edd5ec897d29a9edfd7898f4b0
ishwariN <ianalgirkar@gmail.com>
2022-04-18 03:24:07 -0400

    added lambda code

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import json
+import smtplib
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    send_plain_email(event)
+    #sqs_queue()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
